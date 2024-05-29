44076fe38b922e7df99d7aec2b242815c90b3555
PampariNaresh <pamparinaresh2001@gmail.com>
2024-05-29 12:22:05 +0530

    Home Page

--- lms-frontend/src/config/axiosinstance.js
+++ lms-frontend/src/config/axiosinstance.js
@@ -5,6 +5,7 @@ const BASE_URL = "http://localhost:5000/api/v1";
 const axiosInstance = axios.create();
 
 axiosInstance.defaults.baseURL = BASE_URL;
+//axiosInstance.defaults.timeout = 2500;
 axiosInstance.defaults.withCredentials = true;
 
 export default axiosInstance;
