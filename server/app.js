d082a8d7fdfb252650634967a20f1c86c9b2bfb6
PampariNaresh <pamparinaresh2001@gmail.com>
2024-08-24 12:01:39 +0530

    Deploy

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const corsOptions = {
-    origin: "https://learning-management-system-frontend-5p470uxl0.vercel.app", // specify the allowed origin
+    origin: "https://learning-management-system-frontend-nevtryvij.vercel.app", // specify the allowed origin
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // allowed HTTP methods
     credentials: true,
 
